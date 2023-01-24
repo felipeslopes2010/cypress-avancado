40eb0c79c525eaa7db1645d474e4023fbbcfbff9
Felipe Lopes <felipeslopes2010@hotmail.com>
2023-01-24 17:35:37 -0300

    Adiciona teste de ordenação por Titulo

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -82,8 +82,8 @@ describe('Hacker Stories', () => {
       })
 
       context('List of stories', () => {
+        const stories = require('../fixtures/stories')
         it('shows the right data for all rendered stories', () => {
-          const stories = require('../fixtures/stories')
 
           cy.get('.item')
             .first()
@@ -114,13 +114,33 @@ describe('Hacker Stories', () => {
           cy.get('.item').should('have.length', 1)
         })
 
-        // Since the API is external,
-        // I can't control what it will provide to the frontend,
-        // and so, how can I test ordering?
-        // This is why these tests are being skipped.
-        // TODO: Find a way to test them out.
-        context.skip('Order by', () => {
-          it('orders by title', () => { })
+        context('Order by', () => {
+          it.only('orders by title', () => {
+
+            cy.get('.list-header-button:contains(Title)')
+              .as('titleHeader')
+              .click()
+
+            cy.get('.item')
+              .first()
+              .should('be.visible')
+              .and('contain', stories.hits[0].title)
+
+            cy.get(`.item a:contains(${stories.hits[0].title})`)
+              .should('have.attr', 'href', stories.hits[0].url)
+
+            cy.get('@titleHeader')
+              .click()
+
+            cy.get('.item')
+              .first()
+              .should('be.visible')
+              .should('contain', stories.hits[1].title)
+
+            cy.get(`.item a:contains(${stories.hits[1].title})`)
+              .should('have.attr', 'href', stories.hits[1].url)
+
+          })
 
           it('orders by author', () => { })
 
